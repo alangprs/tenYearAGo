f594e0f67c098d4a9e6f7de973634024232c375f
will <alangprs1@gmail.com>
2020-12-18 18:18:21 +0800

    尚未新增UIDatePicker功能

--- tenYearAGo/ViewController.swift
+++ tenYearAGo/ViewController.swift
@@ -1,19 +1,39 @@
-//
-//  ViewController.swift
-//  tenYearAGo
-//
-//  Created by 翁燮羽 on 2020/12/18.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dataPickerView: UIDatePicker!
+    @IBOutlet weak var sliderView: UISlider!
+    @IBOutlet weak var laberView: UILabel!
+    //讀取系統日期
+    let dateformatter = DateFormatter()
+    //設定變數在@IBAction func changSlider使用
+    var sliderNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        dataPickerView.locale = Locale(identifier: "zh-TW")
+        dateformatter.dateFormat = "yyyy"
     }
-
+   
+    //加入圖片
+    let imageName = ["2009","2010","2012","2013","2014","2015","2016","2017","2018","2019","2020"]
+    
+    @IBAction func changSlider(_ sender: UISlider) {
+        //拉動slider換圖片
+        sliderNumber = Int(sliderView.value)
+        imageView.image = UIImage(named: String(imageName[sliderNumber]))
+       //顯示lable文字
+        laberView.text = String("\(imageName[sliderNumber])年")
+    }
+    @IBAction func changDataPicker(_ sender: Any) {
+        
+    }
+    
+    
 
 }
 
